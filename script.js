25cb0e97d3de9d88b55f9f1c77068df386162593
aprillebalsom <aprillebalsom@gmail.com>
2020-09-04 21:43:21 -0400

    error handling and narrowing user selections

--- script.js
+++ script.js
@@ -149,5 +149,34 @@ const form = $('form');
 //
 $('form').on('submit', function(e) {
     e.preventDefault();
-    console.log('yay');
-});
+    // console.log('yay');
+
+    const cameraPick = $('input[name = camera]:checked').val();
+    // console.log('camera');
+    const filmPick = $('input[name = film]:checked').val();
+    // console.log('film');
+    const subjectPick = $('input[name = subject]:checked').val();
+    // console.log('subject');
+
+    if ($('input[type = radio]') !== $('input[type = radio]:checked')) {
+        alert('oops! looks like your missing some required fields, please fill out all sections and try again')
+    }
+
+
+    const userSelections = [];
+
+    const cameraTypes = filmPhoto[cameraPick];
+    // console.log(cameraTypes);
+
+    for (let i = 0; i < cameraTypes.length; i++) {
+        // console.log(cameraTypes[i]);
+
+        if (filmPick === cameraTypes[i].film && subjectPick === cameraTypes[i].subject) {
+            
+
+
+        }
+    }
+
+
+})
