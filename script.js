94b8cec601b6b84700e5944381a8ffa156eef905
aprillebalsom <aprillebalsom@gmail.com>
2020-09-04 22:05:08 -0400

    added user selection appending to page

--- script.js
+++ script.js
@@ -141,42 +141,49 @@ const filmPhoto = {
 }
 
 
-//TODO -  insert document ready
+$(function(){
 
-const form = $('form');
+    
 
-//on submit, prevent default
-//
-$('form').on('submit', function(e) {
-    e.preventDefault();
-    // console.log('yay');
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        // console.log('yay');
 
-    const cameraPick = $('input[name = camera]:checked').val();
-    // console.log('camera');
-    const filmPick = $('input[name = film]:checked').val();
-    // console.log('film');
-    const subjectPick = $('input[name = subject]:checked').val();
-    // console.log('subject');
+        const cameraPick = $('input[name = camera]:checked').val();
+        // console.log('camera');
+        const filmPick = $('input[name = film]:checked').val();
+        // console.log('film');
+        const subjectPick = $('input[name = subject]:checked').val();
+        // console.log('subject');
 
-    if ($('input[type = radio]') !== $('input[type = radio]:checked')) {
-        alert('oops! looks like your missing some required fields, please fill out all sections and try again')
-    }
+        // if ($('input[type = radio]') !== $('input[type = radio]:checked')) {
+        //     alert('oops! looks like your missing some required fields, please fill out all sections and try again!')
+        // }
 
 
-    const userSelections = [];
+        // const userSelections = [];
 
-    const cameraTypes = filmPhoto[cameraPick];
-    // console.log(cameraTypes);
+        const cameraTypes = filmPhoto[cameraPick];
+        // console.log(cameraTypes);
 
-    for (let i = 0; i < cameraTypes.length; i++) {
-        // console.log(cameraTypes[i]);
+        for (let i = 0; i < cameraTypes.length; i++) {
+            // console.log(cameraTypes[i]);
+            const userSelection = cameraTypes[i];
+            if (filmPick === userSelection.film && subjectPick === userSelection.subject) {
+        
+            //  userSelections.push(cameraTypes[i]);
+            //  console.log(userSelections);
 
-        if (filmPick === cameraTypes[i].film && subjectPick === cameraTypes[i].subject) {
-            
+            $('section').append(`<p> yay </p>`);
 
 
+                
+            }
         }
-    }
+
+      
 
 
-})
+    });
+
+});
